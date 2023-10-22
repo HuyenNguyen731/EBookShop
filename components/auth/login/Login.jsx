f28a93148c3830c392d3ae807b7c014b43484d5d
thuhuyen <nguyenthuhuyen@gmail.com>
2023-10-22 16:21:50 +0700

    update logic login

--- components/auth/login/Login.jsx
+++ components/auth/login/Login.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import { Link } from "expo-router";
+import { Link, useLinkTo } from "expo-router";
 import styles from "./login.style";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const data = {
+        email: email,
+        hashedPassword: password,
+      };
+
+      const response = await axios.post(
+        "https://localhost:7135/api/User/login",
+        data
+      );
+      const token = response?.data?.jwtToken;
+
+      if (token) {
+        await AsyncStorage.setItem("TOKEN", token);
+        linkTo("/home");
+      }
+    } catch (error) {
+      console.error("Lỗi đăng nhập: ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.headText}>Welcome Back!</Text>
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput style={styles.input} placeholder="Password" />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+      />
       <Text style={styles.contextText}>Forgot Password?</Text>
-      <TouchableOpacity style={styles.button}>
-        <Link href="/home" style={styles.btnText}>
-          Login
-        </Link>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.btnText}>Login</Text>
       </TouchableOpacity>
       <View style={styles.footerText}>
         <Text>Create An Account</Text>
