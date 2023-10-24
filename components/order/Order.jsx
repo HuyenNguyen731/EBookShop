fb209f740284879811c6f3ae47a5fb35fa5f559a
thuhuyen <nguyenthuhuyen@gmail.com>
2023-10-24 10:09:35 +0700

    update logic status order

--- components/order/Order.jsx
+++ components/order/Order.jsx
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Dimensions } from "react-native";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
 import CardStatus from "../common/cardstatus/CardStatus";
 import styles from "./order.style";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Pending = () => (
-  <View style={styles.scene}>
-    <CardStatus status="1" />
-    <CardStatus status="1" />
-  </View>
-);
-const Confirmed = () => (
-  <View style={styles.scene}>
-    <CardStatus status="2" />
-  </View>
-);
-const Delivered = () => (
-  <View style={styles.scene}>
-    <CardStatus status="3" />
-  </View>
-);
-const Cancelled = () => (
-  <View style={styles.scene}>
-    <CardStatus status="4" />
-  </View>
-);
+const fetchDataForStatus = async (status, token) => {
+  try {
+    const response = await axios.get(
+      `https://localhost:7135/api/orders/status/${status}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response?.data?.data;
+  } catch (error) {
+    console.error(`Error when calling API for status ${status}: `, error);
+    return [];
+  }
+};
+
+const TabContent = ({ status }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("TOKEN")
+      .then((TOKEN) => {
+        if (TOKEN) {
+          fetchDataForStatus(status, TOKEN).then((statusData) => {
+            setData(statusData);
+          });
+        } else {
+          console.error("Token does not exist");
+        }
+      })
+      .catch((error) => {
+        console.error("Error when getting token from AsyncStorage: ", error);
+      });
+  }, [status]);
+
+  return (
+    <View style={styles.scene}>
+      {data?.map((item, index) => (
+        <CardStatus key={index} status={status} />
+      ))}
+    </View>
+  );
+};
 
 const renderTabBar = (props) => (
   <TabBar
@@ -52,10 +77,10 @@ export default class TabViewExample extends React.Component {
         navigationState={this.state}
         renderTabBar={renderTabBar}
         renderScene={SceneMap({
-          1: Pending,
-          2: Confirmed,
-          3: Delivered,
-          4: Cancelled,
+          1: () => <TabContent status="1" />,
+          2: () => <TabContent status="2" />,
+          3: () => <TabContent status="3" />,
+          4: () => <TabContent status="4" />,
         })}
         onIndexChange={(index) => this.setState({ index })}
         initialLayout={{ width: Dimensions.get("window").width }}
